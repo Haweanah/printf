f463f104cff1db6d93f1148f4ecf3e94430b10e4
Haweanah <ahauwa48@yahoo.com>
2023-01-23 16:49:45 -0800

    functions.c

--- functions.c
+++ functions.c
@@ -26,7 +26,8 @@ width, precision, size));
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width@precision: Precision specification
+ * @width: get width
+ * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
